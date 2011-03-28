2f32a58587403964998ac5c88384e98412db283d
Pringley <ben.pringle@gmail.com>
2011-03-27 20:26:11 -0400

    added error log

--- clserv.py
+++ clserv.py
@@ -2,7 +2,7 @@
 
 import sys, os, cgi, xml.dom.minidom, sqlite3
 
-# uncomment to enable debugging
+# uncomment to enable error output in browser
 sys.stderr = sys.stdout
 
 print 'Content-Type: text/plain'
@@ -11,16 +11,15 @@ print
 # set up CGI (input data)
 field = cgi.FieldStorage()
 
-# set up XML (output data)
-impl  = xml.dom.minidom.getDOMImplementation()
-doc = impl.createDocument(None, 'cls', None)
-top = doc.documentElement
-
 # set up SQL (store data)
 conn = sqlite3.connect('clsql.db')
 curs = conn.cursor()
 curs.execute('create table if not exists main (ip, text)')
 
+# set up XML (output data)
+impl  = xml.dom.minidom.getDOMImplementation()
+doc = impl.createDocument(None, 'cls', None)
+top = doc.documentElement
 def addElement(name, parent, attributes):
     '''Create a DOM element.
     
@@ -36,38 +35,18 @@ def addElement(name, parent, attributes):
     parent.appendChild(element)
     return element
 
-# handle get request
-if 'getip' in field:
-    # input from CGI field
-    ip = field['getip'].value
-
-    # set up XML tag
-    user = addElement('user', top, {'ip':ip})
+# create log element
+log = addElement('log', top, {})
+def addLog(entry):
+    '''Append a string to the log element.
     
-    # append text from SQL data
-    curs.execute('select * from main where ip=?',(ip,))
-    for row in curs:
-        addElement('text', user, {'value':row[1]})
-
-# handle set request
-if 'settext' in field:
-    # get remote IP from environment variables
-    ip = os.getenv('HTTP_CLIENT_IP') or os.getenv('HTTP_X_FORWARDED_FOR') or os.getenv('REMOTE_ADDR') or 'UNKNOWN'
-
-    # input from CGI field
-    text = field['settext'].value
-
-    # set up XML tag
-    user = addElement('user', top, {'ip':ip})
-
-    # add to SQL database
-    curs.execute('insert into main values (?,?)', (ip, text))
-
-    # print all text (including prior insertions)
-    curs.execute('select * from main where ip=?',(ip,))
-    for row in curs:
-        addElement('text', user, {'value':row[1]})
+    Keyword arguments:
+    entry -- a string to be added to the log.
     
+    '''
+    entrynode = doc.createTextNode(entry)
+    log.appendChild(entrynode)
+
 
 # close SQL
 conn.commit()
@@ -75,5 +54,3 @@ curs.close()
 
 # print XML
 print doc.toprettyxml(indent=' '*4,encoding='utf-8')
-
-
